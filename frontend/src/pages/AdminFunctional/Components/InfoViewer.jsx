2c29240616a7836dde5a9fff293d993443a7c0dc
Ruslan Bosin <RuslanBosin28@gmail.com>
2024-01-25 01:19:21 +0300

    admin field editor

---
+++ frontend/src/pages/AdminFunctional/Components/InfoViewer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+// Styles
+const body_div = {
+  background: "lightgrey",
+  minWidth: "250px",
+  maxWidth: "400px",
+  flexGrow: 1, 
+  height: "100%"
+}
+
+function InfoViewer() {
+  return (
+    <div style={body_div}>
+      
+    </div>
+  );
+}
+
+export default InfoViewer;
