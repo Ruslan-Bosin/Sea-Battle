bd55aec0a33bfa0b24a1f627ee8135df651faf77
Ruslan-Bosin <76877334+Ruslan-Bosin@users.noreply.github.com>
2024-02-10 15:00:42 +0300

    Update Forbidden.jsx -403

--- frontend/src/app/UtilitarianPages/Forbidden.jsx
+++ frontend/src/app/UtilitarianPages/Forbidden.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import { Result, Button } from "antd";
 import { useNavigate } from "react-router-dom";
 
-function Unauthorised() {
+function Forbidden() {
 
   const navigate = useNavigate();
 
   return (
     <Result
-      status="401"
-      title="401"
+      status="403"
+      title="403"
       subTitle="К сожалению, у вас нет прав для доступа к этой странице."
       extra={<Button onClick={() => { navigate("/") }} type="primary">На главную</Button>}
     />
   );
 };
 
-export default Unauthorised;
+export default Forbidden;
